e59cd3eec74f0dc22ceed03c2b425e1e8824031e
danilelcasanova12 <snaxofc10@gmail.com>
2024-04-05 16:46:41 -0300

    feat: class transactionService.java

--- bankapi-v2/src/main/java/br/edu/utfpr/bankapi/service/TransactionService.java
+++ bankapi-v2/src/main/java/br/edu/utfpr/bankapi/service/TransactionService.java
@@ -1,12 +1,17 @@
 package br.edu.utfpr.bankapi.service;
 
+import org.aspectj.weaver.ast.Not;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.edu.utfpr.bankapi.dto.DepositDTO;
 import br.edu.utfpr.bankapi.dto.TransferDTO;
 import br.edu.utfpr.bankapi.dto.WithdrawDTO;
+import br.edu.utfpr.bankapi.exception.NotFoundException;
+import br.edu.utfpr.bankapi.model.Account;
 import br.edu.utfpr.bankapi.model.Transaction;
+import br.edu.utfpr.bankapi.model.TransactionType;
+import br.edu.utfpr.bankapi.repository.AccountRepository;
 import br.edu.utfpr.bankapi.repository.TransactionRepository;
 import jakarta.transaction.Transactional;
 
@@ -14,13 +19,34 @@ import jakarta.transaction.Transactional;
 public class TransactionService {
     @Autowired
     private TransactionRepository transactionRepository;
+    @Autowired
+    private AccountRepository accountRepository;
 
     /**
      * Operação de depósito em uma conta
      */
     @Transactional
-    public Transaction deposit(DepositDTO dto) {
-        return null;
+    public Transaction deposit(DepositDTO dto) throws NotFoundException {
+        // faça o codigo para o deposito
+        Transaction transaction = new Transaction(null, null, dto.amount(), TransactionType.DEPOSIT);
+        transactionRepository.save(transaction);
+
+        var res = accountRepository.getByNumber(dto.receiverAccountNumber());
+        // verifica se a conta de destino existe
+        if (res.isEmpty()) {
+            throw new NotFoundException();
+        }
+
+        // verifica se o mount e menor ou = a 0
+        if (dto.amount() <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        // credito na conta de destino
+        transaction.getReceiverAccount().setBalance(
+                transaction.getReceiverAccount().getBalance() + dto.amount());
+
+        return transactionRepository.save(transaction);
     }
 
     /**
@@ -39,4 +65,5 @@ public class TransactionService {
 
         return null;
     }
+
 }
